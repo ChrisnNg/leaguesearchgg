e146cdd2e383742e44243cd321a3468d937ae9ab
ChrisnNg <cristopherng@hotmail.com>
2020-01-13 06:17:07 -0800

    update to hashrouter for netlfiy

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 ReactDOM.render(
   <Router>
