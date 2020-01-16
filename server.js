82f4d341c40aaba513b77b48e06a23616d54edef
ChrisnNg <cristopherng@hotmail.com>
2020-01-15 19:38:02 -0800

    uri encode summonername before search

--- server.js
+++ server.js
@@ -37,6 +37,9 @@ app.get("/", (req, res) => {
 app.post("/summonerSearch", (req, res) => {
   let summoner = req.body.username;
 
+  summoner = encodeURI(summoner);
+
+  console.log("summoner = ", summoner);
   var summonerInfoRequest = {
     method: "GET",
     url: `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summoner}`,
